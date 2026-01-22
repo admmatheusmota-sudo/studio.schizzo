cfb351a5518557d4242722d32973e02f224af5ab
adm.matheus.mota <adm.matheus.mota@gmail.com>
2026-01-22 18:12:14 +0000

    Changes before Firebase Studio auto-run

---
+++ .idx/dev.nix
@@ -0,0 +1,19 @@
+
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.python3
+  ];
+  idx = {
+    extensions = [ ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
---
+++ projects.js
@@ -0,0 +1,40 @@
+
+const projectsData = [
+  {
+    id: 0,
+    title: "Casa L & P",
+    location: "Residencial - Brasília",
+    thumb: "https://i.postimg.cc/sx3Pbz4Y/Casa-1-1.jpg",
+    images: [
+      "https://i.postimg.cc/sx3Pbz4Y/Casa-1-1.jpg",
+      "https://i.postimg.cc/7hq1j4n7/Casa-1-2.jpg",
+      "https://i.postimg.cc/Y0tNT7zm/Casa-1-3.jpg",
+      "https://i.postimg.cc/Kjm7CbDT/Casa-1-4.jpg"
+    ]
+  },
+  {
+    id: 1,
+    title: "House H & P",
+    location: "Residencial - Goiânia",
+    thumb: "https://i.postimg.cc/gJgrkPPJ/Casa-2-1.jpg",
+    images: [
+      "https://i.postimg.cc/gJgrkPPJ/Casa-2-1.jpg",
+      "https://i.postimg.cc/C1hzgx9q/Casa-2-2.jpg",
+      "https://i.postimg.cc/B6SX0nR1/Casa-2-3.jpg",
+      "https://i.postimg.cc/xC6qTSS1/Casa-2-4.jpg",
+      "https://i.postimg.cc/9MLzFjj0/Casa-2-5.jpg"
+    ]
+  },
+  {
+    id: 2,
+    title: "Casa Alfa",
+    location: "Portal do golf - Goiânia",
+    thumb: "https://i.postimg.cc/qMhg0YD7/Projeto-Agnaldo-1-Foto.jpg",
+    images: [
+      "https://i.postimg.cc/qMhg0YD7/Projeto-Agnaldo-1-Foto.jpg",
+      "https://i.postimg.cc/X7GX3h1X/Projeto-Agnaldo-2-Foto.jpg",
+      "https://i.postimg.cc/CLBdYtr5/Projeto-Agnaldo-4-Foto.jpg",
+      "https://i.postimg.cc/RV6q98pZ/Projeto-Agnaldo-5-Foto.jpg"
+    ]
+  }
+];
